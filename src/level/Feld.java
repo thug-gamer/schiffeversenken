db26989d1d5cb6be792fa712b98751ef2fd37e1d
Leandro Baltazar <leandrobaltazar@hotmail.de>
2015-10-23 23:03:17 +0200

    level initial erstellen, methode shoot eingebaut

--- src/level/Feld.java
+++ src/level/Feld.java
@@ -2,8 +2,33 @@ package level;
 
 public class Feld {
 	
+	private boolean istSchiff;
+	private boolean istBeschossen;
+	
 	public Feld() {
-		
+		istSchiff = false;
+		istBeschossen = false;
+	}
+
+	public boolean isIstSchiff() {
+		return istSchiff;
+	}
+
+	public void setIstSchiff(boolean istSchiff) {
+		this.istSchiff = istSchiff;
+	}
+
+	public boolean isIstBeschossen() {
+		return istBeschossen;
+	}
+
+	public void setIstBeschossen(boolean istBeschossen) {
+		this.istBeschossen = istBeschossen;
+	}
+
+	@Override
+	public String toString() {
+		return "[ ]";
 	}
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,24 @@
 package main;
 
+import java.util.Scanner;
+
 import level.Level;
 
 public class Main {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		Level level = new Level();
-		System.out.println(level.getMap());
+		System.out.println(level);
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("enter x");
+		int x = scanner.nextInt();
+		
+		System.out.println("enter y");
+		int y = scanner.nextInt();
+		
+		System.out.println("x: " + x  + ", y: " + y);
 	}
 
 }
