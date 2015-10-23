f73cc359c6b7f52bdf812bc2ed196aad045cad9c
Leandro Baltazar <leandrobaltazar@hotmail.de>
2015-10-24 00:21:30 +0200

    main geändert

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,23 +2,20 @@ package main;
 
 import java.util.Scanner;
 
+import utils.Interpreter;
 import level.Level;
 
 public class Main {
 
-	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		Level level = new Level();
-		System.out.println(level);
 
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("enter x");
-		int x = scanner.nextInt();
-		
-		System.out.println("enter y");
-		int y = scanner.nextInt();
-		
-		System.out.println("x: " + x  + ", y: " + y);
+		while(true) {
+			System.out.println("Befehl:");
+			String input = scanner.next();
+			Interpreter.interpret(input, level, scanner);
+		}
 	}
 
 }
