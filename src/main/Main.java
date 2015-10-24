c797e59bcc466e712ac53a699e3e4b3103a32b2a
Leandro Baltazar <leandrobaltazar@hotmail.de>
2015-10-24 04:56:31 +0200

    fertiges Spiel Version 1.0

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,26 +2,28 @@ package main;
 
 import java.util.Scanner;
 
-import schiffe.Schiff;
-import schiffe.Schlachtschiff;
-import schiffe.Zerstoerer;
-import utils.Interpreter;
 import level.Level;
+import utils.Interpreter;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Level level = new Level();
-
-		Schiff zerstoerer = new Zerstoerer(0, 0, 1);
-		level.addSchiff(zerstoerer);
-		System.out.println(level);
-		level.shoot(1, 0);
-		level.shoot(2, 0);
-		level.shoot(3, 0);
-		level.shoot(4, 0);
-		level.shoot(0, 0);
-		System.out.println(level);
+		level.setAlleSchiffe();
+//		for (int i = 0; i < 10; i++) {
+//			for (int j = 0; j < 10; j++) {
+//				level.shoot(i, j);
+//			}
+//		}
+//		Schiff zerstoerer = new Zerstoerer(0, 0, 1);
+//		level.addSchiff(zerstoerer);
+//		System.out.println(level);
+//		level.shoot(1, 0);
+//		level.shoot(2, 0);
+//		level.shoot(3, 0);
+//		level.shoot(4, 0);
+//		level.shoot(0, 0);
+//		System.out.println(level);
 
 		Scanner scanner = new Scanner(System.in);
 		while(true) {
